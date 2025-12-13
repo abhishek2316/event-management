693e5338d9efaa4c072e8988d5d0351b6534a8ff
abhishek2316 <kumarabhi1623@gmail.com>
2025-12-14 00:24:40 +0530

    feat(entity): add event entity

---
+++ src/main/java/com/abhi/tickets/domain/Event.java
@@ -0,0 +1,69 @@
+package com.abhi.tickets.domain;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "events")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event {
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "start")
+    private LocalDateTime start;
+
+    @Column(name = "end")
+    private LocalDateTime end;
+
+    @Column(name = "venue", nullable=false)
+    private String Venue;
+
+    @Column(name = "sale_start")
+    private LocalDateTime sale_start;
+
+    @Column(name = "sale_end")
+    private LocalDateTime sale_end;
+
+    @Column(name = "status", nullable=false)
+    @Enumerated(EnumType.STRING)
+    private EventStatusEnum status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "organizer_id")
+    private User organizer;
+
+    @ManyToMany(mappedBy = "attendingEvents")
+    private List<User> attendees = new ArrayList<>();
+
+    @ManyToMany(mappedBy = "staffingEvents")
+    private List<User> staffs = new ArrayList<>();
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+}
