9dd8c0569f4837aaf8aa3f91a924ca44750f2968
abhishek2316 <kumarabhi1623@gmail.com>
2025-12-15 00:03:38 +0530

    feat(entity): added the ticket entity and modify the ticketType class

--- src/main/java/com/abhi/tickets/domain/Ticket.java
+++ src/main/java/com/abhi/tickets/domain/Ticket.java
@@ -1,41 +1,43 @@
 package com.abhi.tickets.domain;
 
-
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "ticket_types")
+@Table(name = "tickets")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class TicketType {
+public class Ticket {
 
     @Id
-    @Column(name = "id", nullable = false, updatable = false)
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id",  updatable = false, nullable = false)
     private UUID id;
 
-    @Column(name = "name",  nullable = false)
-    private String name;
-
-    @Column(name = "price", nullable = false)
-    private double price;
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TicketStatusEnum status;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    private Event event;
+    @JoinColumn(name = "ticket_type_id")
+    private TicketType ticketType;
 
-    @OneToMany(mappedBy = "ticketType",  cascade = CascadeType.ALL)
-    private List<Ticket> tickets = new ArrayList<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "purchaser_id")
+    private User purchaser;
+
+//    TODO: Ticket Validation
+
+//    TODO: QR Code
+
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
