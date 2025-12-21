1399b96f3765f36fcf152184cb6d47aac16d9927
abhishek2316 <kumarabhi1623@gmail.com>
2025-12-22 00:04:54 +0530

    fix: remove PostgreSQL reserved keywords and event save error

--- src/main/java/com/abhi/tickets/services/impl/EventServiceImpl.java
+++ src/main/java/com/abhi/tickets/services/impl/EventServiceImpl.java
@@ -24,6 +24,7 @@ public class EventServiceImpl implements EventService {
         userRepository.findById(organizerId).orElseThrow( () -> new UserNotFoundException(
                 String.format("User with id %s not found", organizerId)
         ));
+        Event eventToCreate = new Event();
 
         List<TicketType> ticketTypesToCreate = event.getTicketTypes().stream().map(
                 ticketType -> {
@@ -32,11 +33,11 @@ public class EventServiceImpl implements EventService {
                     ticketTypeToCreate.setPrice(ticketType.getPrice());
                     ticketTypeToCreate.setDescription(ticketType.getDescription());
                     ticketTypeToCreate.setTotalAvailable(ticketType.getTotalAvailable());
+                    ticketTypeToCreate.setEvent(eventToCreate);
                     return ticketTypeToCreate;
                 }
         ).toList() ;
 
-        Event eventToCreate = new Event();
         eventToCreate.setName(event.getName());
         eventToCreate.setStart(event.getStart());
         eventToCreate.setEnd(event.getEnd());
