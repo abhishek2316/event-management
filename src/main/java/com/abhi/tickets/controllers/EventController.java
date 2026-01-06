9d4ee05e328ee142b2dd771103762a8ad1ce35b8
abhishek2316 <kumarabhi1623@gmail.com>
2026-01-06 22:01:45 +0530

    feat: added the list event endpoint with EventsController

--- src/main/java/com/abhi/tickets/controllers/EventController.java
+++ src/main/java/com/abhi/tickets/controllers/EventController.java
@@ -4,18 +4,19 @@ import com.abhi.tickets.domain.CreateEventRequest;
 import com.abhi.tickets.domain.Event;
 import com.abhi.tickets.domain.dtos.CreateEventRequestDto;
 import com.abhi.tickets.domain.dtos.CreateEventResponseDto;
+import com.abhi.tickets.domain.dtos.ListEventResponseDto;
 import com.abhi.tickets.mappers.EventMapper;
 import com.abhi.tickets.services.EventService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.UUID;
@@ -32,11 +33,23 @@ public class EventController {
     public ResponseEntity<CreateEventResponseDto> createEvent(
             @AuthenticationPrincipal Jwt jwt, @Valid @RequestBody CreateEventRequestDto createEventRequestDto) {
         CreateEventRequest createEventRequest = eventMapper.fromDto(createEventRequestDto);
-        UUID userId = UUID.fromString(jwt.getSubject());
+//        UUID userId = UUID.fromString(jwt.getSubject());
+        UUID userId = parseUUID(jwt);
         Event createEvent = eventService.createEvent(userId, createEventRequest);
         CreateEventResponseDto createEventResponseDto = eventMapper.toDto(createEvent);
         return new  ResponseEntity<>(createEventResponseDto, HttpStatus.CREATED);
     }
 
+    @GetMapping
+    public ResponseEntity<Page< ListEventResponseDto>> listEvents(
+            @AuthenticationPrincipal Jwt jwt, Pageable pageable
+    ){
+        UUID userId = parseUUID(jwt);
+        Page<Event> events =  eventService.listEventsForOrganizer(userId, pageable );
+        return ResponseEntity.ok(events.map(eventMapper::toListEventResponseDto));
+    }
 
+    private UUID parseUUID(Jwt jwt) {
+        return UUID.fromString(jwt.getSubject());
+    }
 }
--- src/main/java/com/abhi/tickets/services/EventService.java
+++ src/main/java/com/abhi/tickets/services/EventService.java
@@ -10,5 +10,5 @@ import java.util.UUID;
 
 public interface EventService {
     Event createEvent(UUID organizerId, CreateEventRequest event);
-    Page<Event> ListEventOrganizer(UUID organizerId,  Pageable pageable);
+    Page<Event> listEventsForOrganizer(UUID organizerId,  Pageable pageable);
 }
