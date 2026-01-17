58cbf9f91a00ba93de6f78cec6239372d372ac35
abhishek2316 <kumarabhi1623@gmail.com>
2026-01-17 21:48:17 +0530

    feat: Handle the exceptions

--- src/main/java/com/abhi/tickets/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/abhi/tickets/controllers/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.abhi.tickets.controllers;
 
 import com.abhi.tickets.domain.dtos.ErrorDto;
+import com.abhi.tickets.exceptions.EventNotFoundException;
+import com.abhi.tickets.exceptions.EventUpdateException;
+import com.abhi.tickets.exceptions.TicketTypeNotFoundException;
 import com.abhi.tickets.exceptions.UserNotFoundException;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +21,29 @@ import java.util.List;
 @Slf4j
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(EventUpdateException.class)
+    public ResponseEntity<ErrorDto> EventUpdateException(EventUpdateException ex) {
+        log.error("Caught Event not update Error", ex);
+        ErrorDto errorDto = new ErrorDto();
+        errorDto.setError("Event not update");
+        return  new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(TicketTypeNotFoundException.class)
+    public ResponseEntity<ErrorDto> TicketTypeNotFoundException(TicketTypeNotFoundException ex) {
+        log.error("Caught TicketType not found Error", ex);
+        ErrorDto errorDto = new ErrorDto();
+        errorDto.setError("Ticket not found");
+        return  new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EventNotFoundException.class)
+    public ResponseEntity<ErrorDto> EventNotFoundException(EventNotFoundException ex) {
+        log.error("Caught event not found Error", ex);
+        ErrorDto errorDto = new ErrorDto();
+        errorDto.setError("Event not found");
+        return  new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorDto> handleUserNotFoundException(UserNotFoundException ex) {
         log.error("Caught user not found Error", ex);
         ErrorDto errorDto = new ErrorDto();
